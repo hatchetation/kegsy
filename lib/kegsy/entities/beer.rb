c24ccf095ee42088e11f253aa18e34517ba1bf43
kellydunn <defaultstring@gmail.com>
2014-03-23 15:25:10 -0700

    Removing uncessary parts of infractructure code, restructuring model relationships a bit

--- lib/kegsy/entities/beer.rb
+++ lib/kegsy/entities/beer.rb
@@ -3,43 +3,46 @@ require 'sequel'
 
 # name string
 # on_tap bool
-class Beer < Sequel::Model
-  one_to_many :servings
-  plugin :json_serializer
+module Kegsy
+  module Entities
+    
+    class Beer < Sequel::Model
+      plugin :json_serializer
 
-  def self.find_by_name(name)
-    return DB["beers"].where({:name => name}).first
-  end
+      one_to_many :keg
+      one_to_many :servings
+      
+      
+      def self.find_by_name(name)
+        return self.where({:name => name}).first
+      end
+      
+      def self.find_or_create_by_name(name)
+        beer = self.find_by_name(name)
+        
+        if beer.nil?
+          beer = self.create({:name => name})
+        end
+        
+        return beer
+      end
+      
+      def self.on_tap
+        return self.where({:on_tap => true})
+      end
+      
+      # TODO pagination
+      def self.to_index_beers_json
+        config = {
+          :include => [
+            :servings
+          ]
+        }
 
-  def self.find_or_create_by_name(name)
-    beer = self.find_by_name(name)
-
-    if beer.nil?
-      beer = self.create({:name => name})
+        self.to_json(config)
+      end
+      
     end
     
-    return beer
   end
-
-  def self.on_tap
-    return Beer.where({:on_tap => true})
-  end
-
-  # TODO pagination
-  def self.to_index_beer_json
-    config = {
-      :include => {
-        :kegs => {
-          :include => {
-            :lines => {
-              :include => {
-                :servings => {}
-              }
-            }
-          }
-        }
-      }
-    }
-  end
-
 end
--- lib/kegsy/entities/serving.rb
+++ lib/kegsy/entities/serving.rb
@@ -1,6 +1,15 @@
-class Serving < Sequel::Model
-  plugin :json_serializer
+require 'sequel'
 
-  many_to_one :beer
+module Kegsy
+  module Entities
 
+    class Serving < Sequel::Model
+      plugin :json_serializer
+
+      many_to_one :beer
+      many_to_one :keg
+      many_to_one :line
+
+    end
+  end
 end
