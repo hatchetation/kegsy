f51750cc9573785f5a2104e7b46be5614fb558ac
kellydunn <defaultstring@gmail.com>
2014-03-26 21:27:17 -0700

    Adding in basic implementation of a message that we will receive from the arduino

--- lib/kegsy/adapters/arduino/serial/protocol.rb
+++ lib/kegsy/adapters/arduino/serial/protocol.rb
@@ -4,6 +4,16 @@ module Kegsy
       module Serial 
 
         class Protocol
+          
+          Message = Struct.new(:event_id, :tap_id, :pour_sequence_id, :duration, :details, :received_at)
+          
+          # @return {Kegsy::Adapters::Arduino::Serial::Protocol::Message}.
+          def self.message_from_payload(payload)
+            
+            
+            Message.new(event_id, tap_id, pour_sequence_id, duration, details, Time.now)
+          end
+          
           # TODO 
           #   - Define protocol
           #   - Parse incoming byte streams
